a3272d6055e4cf807ecfdf2e8c9ddae3c888f164
WenZheng Li <me@wenzheng.li>
2019-04-15 20:47:18 +0900

    changed player to dynamically load character

--- little_fighter_z/logic/player.gd
+++ little_fighter_z/logic/player.gd
@@ -1,27 +1,35 @@
 extends Node
 
 export(int) var input_id := 0
-export(String) var player_name := "Player0"
-	
+export(PackedScene) var character_scene := preload("res://characters/character.tscn")
+
+var character : Character
+
+func _ready(): 
+	character = character_scene.instance() as Character
+	add_child(character)
+
+func set_character(new_character_scene : PackedScene):
+	character.queue_free()
+	character_scene = new_character_scene
+	character = character_scene.instance() as Character
+	add_child(character)
+		
 func _process(delta):
-	if not $Character:
-		return
-	$Character.velocity_request = Vector3.ZERO
+	character.velocity_request = Vector3.ZERO
 	if Input.is_action_pressed(str(input_id) + "_left"):
-		$Character.velocity_request.x -= 1
+		character.velocity_request.x -= 1
 	elif Input.is_action_pressed(str(input_id) + "_right"):
-		$Character.velocity_request.x += 1
+		character.velocity_request.x += 1
 	if Input.is_action_pressed(str(input_id) + "_up"):
-		$Character.velocity_request.z -= 1
+		character.velocity_request.z -= 1
 	elif Input.is_action_pressed(str(input_id) + "_down"):
-		$Character.velocity_request.z += 1
+		character.velocity_request.z += 1
 		
 func _unhandled_key_input(event):
-	if not $Character:
-		return
 	if event.is_action_pressed(str(input_id) + "_attack"):
-		$Character.input_action(Character.Action.ATTACK)
+		character.input_action(Character.Action.ATTACK)
 	elif event.is_action_pressed(str(input_id) + "_defend"):
-		$Character.input_action(Character.Action.DEFEND)
+		character.input_action(Character.Action.DEFEND)
 	elif event.is_action_pressed(str(input_id) + "_jump"):
-		$Character.input_action(Character.Action.JUMP)
+		character.input_action(Character.Action.JUMP)
