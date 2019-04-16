f5e56043fdc837adfac3797b92f5cd326aabde75
WenZheng Li <me@wenzheng.li>
2019-04-16 13:42:25 +0900

    minor movement fixes and refactoring

--- little_fighter_z/characters/character.gd
+++ little_fighter_z/characters/character.gd
@@ -5,6 +5,8 @@ class_name Character
 export(float) var walk_speed: float = 2
 export(float) var run_speed: float = 4
 
+enum State {REST, WALK, RUN, PAIN}
+
 enum ComboKey {NONE, UP, DOWN, LEFT, RIGHT, ATTACK, DEFEND, JUMP}
 
 enum {
@@ -12,51 +14,48 @@ enum {
 	D = ComboKey.DOWN,
 	L = ComboKey.LEFT,
 	R = ComboKey.RIGHT,
-	LL = (ComboKey.LEFT << 3) | ComboKey.LEFT,
-	LLL = (LL << 3) | ComboKey.LEFT,
-	RR = (ComboKey.RIGHT << 3) | ComboKey.RIGHT,
-	RRR = (RR << 3) | ComboKey.RIGHT,
+	A = ComboKey.ATTACK,
+	B = ComboKey.DEFEND,
+	C = ComboKey.JUMP,
+	UU = (U << 3) | U,
+	DD = (D << 3) | D,
+	LL = (L << 3) | L,
+	RR = (R << 3) | R,
+	UUU = (UU << 3) | U,
+	DDD = (DD << 3) | D,
+	LLL = (LL << 3) | L,
+	RRR = (RR << 3) | R,
 }
-
-func combo_string(combo_code : int) -> String:
-	var combo_string : String
-	for i in range(3):
-		match (combo_code >> ((2 - i) * 3)) & 0x7:
-			0:
-				combo_string += '_'
-			1:
-				combo_string += 'U'
-			2:
-				combo_string += 'D'
-			3:
-				combo_string += 'L'
-			4:
-				combo_string += 'R'
-			5:
-				combo_string += 'A'
-			6:
-				combo_string += 'D'
-			7:
-				combo_string += 'J'
-	return combo_string
-
-enum State {REST, WALK, RUN, PAIN}
 
 onready var animation_player := $AnimationPlayer as AnimationPlayer
 onready var sprite_3d := $Sprite3D as Sprite3D
 
-var state := 0
+var state : int = State.REST
 var velocity := Vector3()
 var velocity_request := Vector3()
 
-func _process(delta : float):
+func _ready():
+	animation_player.play("Rest")
+
+func _physics_process(delta : float):
 	if state == State.WALK and velocity_request.x == 0 and velocity_request.z == 0:
-		state = State.REST
+		change_state(State.REST)
+	match state:
+		State.WALK:
+			velocity = move_and_slide(velocity_request.normalized() * walk_speed)
+		State.RUN:
+			velocity_request.x = sign(velocity.x)
+			velocity = move_and_slide(velocity_request.normalized() * run_speed)
 	if velocity.x != 0 or velocity.z != 0:
 		if velocity.x < 0:
 			sprite_3d.flip_h = true
 		elif velocity.x > 0:
 			sprite_3d.flip_h = false
+	
+func change_state(new_state : int) -> void:
+	if state == new_state:
+		return
+	state = new_state
 	match state:
 		State.REST:
 			animation_player.play("Rest")
@@ -64,37 +63,31 @@ func _process(delta : float):
 			animation_player.play("Walk")
 		State.RUN:
 			animation_player.play("Run")
-		
-func _physics_process(delta : float):
-	match state:
-		State.WALK:
-			velocity = move_and_slide(velocity_request.normalized() * walk_speed)
-		State.RUN:
-			velocity_request.x = sign(velocity.x)
-			velocity = move_and_slide(velocity_request.normalized() * run_speed)
+		State.PAIN:
+			animation_player.play("Pain")
 
-func input_combo(combo_code : int):
-	print(combo_string(combo_code))
+
+func input_combo(combo_code : int) -> void:
 	match combo_code:
-		U, D, L, R:
+		U, D, L, R, UU, UUU, DD, DDD:
 			_combo_walk(combo_code)
 		LL, LLL, RR, RRR:
 			_combo_run(combo_code)
 
-func _combo_walk(combo_code):
+func _combo_walk(combo_code : int) -> void:
 	match state:
 		State.REST, State.WALK:
-			state = State.WALK
+			change_state(State.WALK)
 		State.RUN:
 			match combo_code:
 				L, LL, LLL:
 					if velocity.x > 0:
-						state = State.WALK
+						change_state(State.WALK)
 				R, RR, RRR:
 					if velocity.x < 0:
-						state = State.WALK
+						change_state(State.WALK)
 	
-func _combo_run(combo_code):
+func _combo_run(combo_code : int) -> void:
 	match state:
 		State.REST, State.WALK, State.RUN:
 			match combo_code:
@@ -102,4 +95,4 @@ func _combo_run(combo_code):
 					velocity = Vector3.LEFT
 				RR, RRR:
 					velocity = Vector3.RIGHT
-			state = State.RUN
+			change_state(State.RUN)
--- little_fighter_z/logic/player.gd
+++ little_fighter_z/logic/player.gd
@@ -2,6 +2,7 @@ extends Node
 
 export(int) var input_id := 0
 export(PackedScene) var character_scene := preload("res://characters/character.tscn")
+export(bool) var print_combo := false
 
 onready var character := character_scene.instance() as Character
 onready var combo_timer := $ComboTimer as Timer
@@ -11,13 +12,7 @@ var combo_code : int = Character.ComboKey.NONE
 func _ready(): 
 	add_child(character)
 
-func set_character(new_character_scene : PackedScene):
-	character.queue_free()
-	character_scene = new_character_scene
-	character = character_scene.instance() as Character
-	add_child(character)
-		
-func _process(delta : float):
+func _physics_process(delta : float):
 	character.velocity_request = Vector3.ZERO
 	if Input.is_action_pressed(str(input_id) + "_left"):
 		character.velocity_request.x -= 1
@@ -27,8 +22,33 @@ func _process(delta : float):
 		character.velocity_request.z -= 1
 	elif Input.is_action_pressed(str(input_id) + "_down"):
 		character.velocity_request.z += 1
-		
-func add_combo_key():
+
+func _unhandled_key_input(event : InputEventKey):
+	if combo_timer.wait_time - combo_timer.time_left < 0.05:
+		combo_code = Character.ComboKey.NONE
+	if not add_combo_key():
+	    return
+	combo_timer.start()
+	combo_code = combo_code & 0x1FF
+	character.input_combo(combo_code)
+	if print_combo:
+		print("%d %s: %s" % [input_id, name, combo_string(combo_code)])
+	combo_code = (combo_code << 3)
+
+func _on_ComboTimer_timeout():
+	combo_code = Character.ComboKey.NONE
+
+func set_character(new_character_scene : PackedScene) -> bool:
+	var new_character := new_character_scene.instance() as Character
+	if not new_character:
+		return false
+	character.queue_free()
+	character = new_character
+	character_scene = new_character_scene
+	add_child(character)
+	return true
+
+func add_combo_key() -> bool:
 	if Input.is_action_pressed(str(input_id) + "_attack"):
 		combo_code += Character.ComboKey.ATTACK
 	elif Input.is_action_pressed(str(input_id) + "_defend"):
@@ -46,16 +66,26 @@ func add_combo_key():
 	else:
 	    return false
 	return true
-	
-func _unhandled_key_input(event : InputEventKey):
-	if combo_timer.wait_time - combo_timer.time_left < 0.05:
-		combo_code = Character.ComboKey.NONE
-	if not add_combo_key():
-	    return
-	combo_timer.start()
-	combo_code = combo_code & 0x1FF
-	character.input_combo(combo_code)
-	combo_code = (combo_code << 3)
 
-func _on_ComboTimer_timeout():
-	combo_code = Character.ComboKey.NONE
+func combo_string(combo_code : int) -> String:
+	var combo_string := "???"
+	for i in range(3):
+		match combo_code & 0x7:
+			Character.ComboKey.NONE:
+				combo_string[i] = '_'
+			Character.ComboKey.UP:
+				combo_string[i] = '^'
+			Character.ComboKey.DOWN:
+				combo_string[i] = 'v'
+			Character.ComboKey.LEFT:
+				combo_string[i] = '<'
+			Character.ComboKey.RIGHT:
+				combo_string[i] = '>'
+			Character.ComboKey.Attack:
+				combo_string[i] = 'A'
+			Character.ComboKey.DEFEND:
+				combo_string[i] = 'D'
+			Character.ComboKey.JUMP:
+				combo_string[i] = 'J'
+		combo_code = combo_code >> 3
+	return combo_string
